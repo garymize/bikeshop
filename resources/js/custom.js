9ae5d549c506fddd2d6357ab34a79f99110787cd
Gary Mize <garymize@Garys-MacBook-Pro.local>
2023-04-12 14:35:28 -0600

    initial home page view and supporting files and custom js files

---
+++ resources/js/custom.js
@@ -0,0 +1,16 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+
+function hello(name) {
+  let phrase = `Hello, ${name}!`;
+
+  say(phrase);
+}
+
+function say(phrase) {
+  alert(`** ${phrase} **`);
+}
+
