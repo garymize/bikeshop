a5c9d67ad302f796f332daf9426ea2478e1dbcf2
Gary Mize <garymize@yahoo.com>
2023-04-15 14:34:41 -0600

    jquery, tooltipster

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
     <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="css/tooltipster.css" />
-    <script src="js/tooltipster.min.js"></script>
+    <script src="js/tooltipster.main.min.js"></script>
     <script src='js/slm.js'></script>
 
     <p>This is my body content.</p>
