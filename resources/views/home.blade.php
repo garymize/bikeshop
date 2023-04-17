82044f15db3ac66a1ce59d0717e1e193e74bd2fe
Gary Mize <garymize@yahoo.com>
2023-04-17 09:52:22 -0600

    buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,7 @@
         // setTimeout(removeIGSMessage,6000);
     </script>
     
+    <button class="IGSbutton menuPointer button-blue" onClick="removeIGSMessage();" style="text-decoration: none; margin-top: 15px; margin-bottom: 15px; max-height: 21px; float: right; margin-left: 10px;">Show Message</button>
     <button class="IGSbutton menuPointer" onClick="removeIGSMessage();" style="text-decoration: none; margin-top: 15px; margin-bottom: 15px; max-height: 21px;">Clear Message</button>
     
 @stop
