bb65d99244d99267f6e64a760b36328681b855fc
Gary Mize <garymize@yahoo.com>
2023-04-20 12:13:59 -0600

    main page

---
+++ resources/views/sandbox.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.layoutHome')
+ 
+@section('title', 'Page Title')
+ 
+@section('sidebar')
+    @parent
+ 
+    <p>This is appended to the master sidebar.</p>
+@stop
+ 
+@section('content')
+
+<body>
+
+    
+    
+    <p>Sandbox</p>
+    
+    <div id="igsDialog"></div>
+    
+    <script>
+        igsMessage('Loading...');
+        // setTimeout(removeIGSMessage,6000);
+    </script>
+    
+    <button id="showMessageButton" class="IGSbutton IGSbutton-green IGSbutton-standard menuPointer button-blue" onClick="igsMessage('Loading...');" style="margin-top: 75px;"> Show Message</button><br>
+    <button id="clearMessageButton" class="IGSbutton IGSbutton-green IGSbutton-standard menuPointer" onClick="removeIGSMessage();" style="width: 200px;">Clear Message</button>
+    <button id="toggleMessageButton" class="IGSbutton IGSbutton-green IGSbutton-standard menuPointer" onClick="toggleIGSMessage();" style="width: 200px; margin-top: 105px;">Toggle Message</button>
+    <button id="disableButtonButton" class="IGSbutton IGSbutton-green IGSbutton-standard menuPointer" onClick="disableButton('showMessageButton');" style="width: 200px; margin-top: 155px;">Disable Button</button>
+    <button id="enableButtonButton" class="IGSbutton IGSbutton-green IGSbutton-standard menuPointer" onClick="enableButton('showMessageButton');" style="width: 200px; margin-top: 205px;">Enable Button</button>
+    <button id="toggleButtonButton" class="IGSbutton IGSbutton-green IGSbutton-standard menuPointer" onClick="toggleButton('showMessageButton');" style="width: 200px; margin-top: 255px;">Toggle Button</button>
+
+</body>
+    
+@stop
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/sandbox', function () {
+        return view('sandbox');
+});
