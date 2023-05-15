562168c541d5dc21228b0a49a756bcfc18195a19
Gary Mize <garymize@yahoo.com>
2023-05-15 16:39:16 -0600

    db controllers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -10,7 +10,7 @@ function bikershop_now(){
 function pg_date($date){
     
     $date = '15-05-2023 10:08 PM';
-    $date = 'Thu, 26 Feb 2015 11:39:59';
+//    $date = 'Thu, 26 Feb 2015 11:39:59';
     $date2 = new DateTime($date);
     return $date2->format('Y-m-d H:i:s');
     
