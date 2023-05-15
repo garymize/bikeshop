c68179f3979f58185c6b1f09253b1f063874ee89
Gary Mize <garymize@yahoo.com>
2023-05-15 16:29:04 -0600

    db controllers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
     
     
     <p>This is my body content.{{ bikershop_now() }}</p>
-    <p>This is my body content.{{ pg_date(bikershop_now()) }}</p>
+    <p>This is my body content.{{ //pg_date(bikershop_now()) }}</p>
     
     <div id="igsDialog"></div>
     
