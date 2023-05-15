7fd551c204c5ca2bbeb391097af141d29eca24d0
Gary Mize <garymize@yahoo.com>
2023-05-15 16:18:08 -0600

    db controllers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -11,7 +11,7 @@ function pg_date($date){
     
 //    $date = '15-05-2023 10:08 PM';
 //    $date = 'Thu, 26 Feb 2015 11:39:59';
-    $date = new DateTime($date);
-    return $date->format('Y-m-d H:i:s');
+    $date2 = new DateTime($date);
+    return $date2->format('Y-m-d H:i:s');
     
 }
