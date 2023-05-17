5dee4de05563f462159e84123a2bc44ced1abf7f
Gary Mize <garymize@yahoo.com>
2023-05-17 15:40:26 -0600

    userForm

--- resources/views/forms/userForm.blade.php
+++ resources/views/forms/userForm.blade.php
@@ -1,8 +1,9 @@
 @section('content')
 
 <body>
+    <p>This is the body content.</p>
     <p>This is my body content.{{ bikershop_now() }}</p>
-    <p>This is my body content.{{ pg_date(bikershop_now()) }}</p>    
+    <p>This is my body content.{{ pg_date(bikershop_now()) }}</p>
 </body>
 
 @stop
