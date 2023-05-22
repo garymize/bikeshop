d2275289328fc177c7a9c94f4d68c2e50ad804dc
Gary Mize <garymize@yahoo.com>
2023-05-22 13:08:14 -0600

    new user form

--- resources/views/forms/newUserForm.blade.php
+++ resources/views/forms/newUserForm.blade.php
@@ -15,11 +15,11 @@
         <p>This is the body content.</p>
         <p>This is my body content.{{ bikershop_now() }}</p>
         <p>This is my body content.{{ pg_date(bikershop_now()) }}</p><br><br>
-        <input type='text' name="username" placeholder='user name' size='50' /> <br><br>
-        <input type='password' name="password" placeholder='password' size='50' /> <br><br>
-        <input type='password' name="confirmpassword" placeholder='confirm password' size='50' /> <br><br>
-        <input type='text' name="email" placeholder='email' size='50' /> <br><br>
-        <input type='text' name="phone" placeholder='phone' size='50' /> <br><br>
+        <input type='text' name="username" placeholder='user name' size='50' required/> <br><br>
+        <input type='password' name="password" placeholder='password' size='50' required/> <br><br>
+        <input type='password' name="confirmpassword" placeholder='confirm password' size='50' required/> <br><br>
+        <input type='text' name="email" placeholder='email' size='50' required/> <br><br>
+        <input type='text' name="phone" placeholder='phone' size='50' required/> <br><br>
         <input type='submit' value="Create User"/>
     </form>
 </body>
