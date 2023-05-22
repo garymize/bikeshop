85996825b78942e1941c3e62e49d41951583e602
Gary Mize <garymize@yahoo.com>
2023-05-22 10:35:56 -0600

    new user form

--- resources/views/forms/newUserForm.blade.php
+++ resources/views/forms/newUserForm.blade.php
@@ -9,11 +9,17 @@
         <p>This is my body content.{{ bikershop_now() }}</p>
         <p>This is my body content.{{ pg_date(bikershop_now()) }}</p><br><br>
         <input type='text' name="username" placeholder='user name' size='50' /> <br><br>
-        <input type='text' name="password" placeholder='password' size='50' /> <br><br>
+        <input type='password' name="password" placeholder='password' size='50' /> <br><br>
         <input type='text' name="email" placeholder='email' size='50' /> <br><br>
         <input type='text' name="phone" placeholder='phone' size='50' /> <br><br>
         <input type='submit' value="Create User"/>
     </form>
 </body>
+<style>
+    input {
+        height: 50px;
+        font-size: 40px;
+    }
+</style>
 
 @stop
