b873c38727ad7b5d37bf2a5fc5a84c2a980ccdff
Gary Mize <garymize@yahoo.com>
2023-05-22 10:37:58 -0600

    new user form

--- resources/views/forms/newUserForm.blade.php
+++ resources/views/forms/newUserForm.blade.php
@@ -3,6 +3,13 @@
 
 @section('content')
 
+<style>
+    input {
+        height: 50px;
+        font-size: 40px;
+    }
+</style>
+
 <body>
     <form action='user/add' method='post'>
         <p>This is the body content.</p>
@@ -10,16 +17,11 @@
         <p>This is my body content.{{ pg_date(bikershop_now()) }}</p><br><br>
         <input type='text' name="username" placeholder='user name' size='50' /> <br><br>
         <input type='password' name="password" placeholder='password' size='50' /> <br><br>
+        <input type='password' name="confirmpassword" placeholder='confirm password' size='50' /> <br><br>
         <input type='text' name="email" placeholder='email' size='50' /> <br><br>
         <input type='text' name="phone" placeholder='phone' size='50' /> <br><br>
         <input type='submit' value="Create User"/>
     </form>
 </body>
-<style>
-    input {
-        height: 50px;
-        font-size: 40px;
-    }
-</style>
 
 @stop
