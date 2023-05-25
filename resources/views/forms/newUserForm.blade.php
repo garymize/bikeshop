0351c62607ec074c7fbb856469a46be5c1807c7c
Gary Mize <garymize@yahoo.com>
2023-05-25 10:07:31 -0600

    new user form

--- resources/views/forms/newUserForm.blade.php
+++ resources/views/forms/newUserForm.blade.php
@@ -22,7 +22,7 @@
         <p>This is the body content.</p>
         <p>This is my body content.{{ bikershop_now() }}</p>
         <p>This is my body content.{{ pg_date(bikershop_now()) }}</p><br><br>
-        <label for='firstname' class='required'>First Name</label><br>
+        <label for='firstname' class='required innerLabel'>First Name</label><br>
         <input type='text' name="firstname" placeholder='' size='50' required/> <br><br>
         <label for='lastname' class='required'>Last Name</label><br>
         <input type='text' name="lastname" placeholder='' size='50' required/> <br><br>
