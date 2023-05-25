408ad7067f0877467b124713c363513e2bba5426
Gary Mize <garymize@yahoo.com>
2023-05-25 13:22:50 -0600

    new user form

--- resources/views/layouts/formsLayout.blade.php
+++ resources/views/layouts/formsLayout.blade.php
@@ -29,7 +29,7 @@
     </head>
     <body>
         @section('sidebar')
-            This is the master sidebar.
+            <!--This is the master sidebar.-->
         @show
  
         <div class="container">
