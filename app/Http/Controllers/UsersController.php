16164227849a7483e630205bb94e5045b44424d2
Gary Mize <garymize@yahoo.com>
2023-05-25 09:53:29 -0600

    new user form

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,7 +19,7 @@ class UsersController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(): view
+    public function create(): View
     {
         return view('forms.newUserForm');
     }
