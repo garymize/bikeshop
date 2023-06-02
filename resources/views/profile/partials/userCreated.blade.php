995d35aa687c4c96d94752831da18a2a7f1bcd75
Gary Mize <garymize@yahoo.com>
2023-06-01 23:58:03 -0600

    csrf

---
+++ resources/views/profile/partials/userCreated.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.formsLayout') 
+@section('title', 'New User Created')
+
+@section('mm-header')
+    <div>Create New User</div>
+@stop
+
+@section('mm-content')
+
+<style>
+/*    input {
+        line-height: 50px;
+        font-size: 40px;
+    }*/
+    
+/*    [required] label::after {
+      content: '*';
+      font-size: 24px;
+      line-height: 0;
+      vertical-align: middle;
+    }*/
+</style>
+
+    <div style="height: 50px; width: 100px; border-style: solid; border-width: thin;">
+        <p> New User Created Successfully</p>
+    </div>
+
+@stop
