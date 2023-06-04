3c743a40e5d4abbc1fc71f2e0b3e712552a6e328
Gary Mize <garymize@yahoo.com>
2023-06-03 20:12:11 -0600

    userView

--- resources/views/userView.blade.php
+++ resources/views/userView.blade.php
@@ -21,17 +21,17 @@
 
     <div>
         <label for='firstname' class='required innerLabel'>First Name</label><br>
-        <input type='text' name="firstname" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
+        <input type='text' autocomplete="off" value="{{ $data->firstname }}" name="firstname" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
         <label for='lastname' class='required innerLabel'>Last Name</label><br>
-        <input type='text' name="lastname" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
+        <input type='text' autocomplete="off" name="lastname" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
         <label for='password' class='required innerLabel'>Password</label><br>        
-        <input type='password' name="password" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
+        <input type='password' autocomplete="off" name="password" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
         <label for='confirmpassword' class='required innerLabel'>Confirm Password</label><br>        
-        <input type='password' name="confirmpassword" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
+        <input type='password' autocomplete="off" name="confirmpassword" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
         <label for='email' class='required innerLabel'>Email</label><br>        
-        <input type='text' name="email" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
+        <input type='text' autocomplete="off" name="email" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br>
         <label for='phone' class='required innerLabel'>Phone</label><br>        
-        <input type='text' name="phone" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br><br>
+        <input type='text' autocomplete="off" name="phone" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br><br>
         <input type='button' style='display:inline-block; float:right;' formaction="{{ route('userEdit') }}" value="Edit"/>
     </div>
     @csrf
