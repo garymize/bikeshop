bdf8234060c9cc65728df6f4e34d125fa4d30203
Gary Mize <garymize@yahoo.com>
2023-06-05 09:45:07 -0600

    show cancel button action

--- resources/views/userView.blade.php
+++ resources/views/userView.blade.php
@@ -33,7 +33,7 @@
         <label for='phone' class='required innerLabel'>Phone</label><br>        
         <input type='text' autocomplete="off" value="{{ phoneFormat($data->phone) }}" name="phone" placeholder='' size='50' class='std-field-width inputDisabled' disabled="true" required/> <br><br>
         <input type='button' style='display:inline-block; float:left;' formaction="{{ route('userEdit') }}" value="Edit"/>
-        <input type='button' style='display:inline-block; float:right;' formaction="{{ route('/home') }}" value="Cancel"/>
+        <input type='button' style='display:inline-block; float:right;' formaction="{{ route('home') }}" value="Cancel"/>
     </div>
     @csrf
 
