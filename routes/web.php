ccfe7d6544e99b4f61cb30bc8828360fcb4d915a
Gary Mize <garymize@yahoo.com>
2023-06-05 09:21:17 -0600

    userView

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home')->name('home');
-});
+    return view('home');
+})->name('home');
 
 Route::get('/home', function () {
     return view('home');
