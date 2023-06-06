fea56ff2f191cdd211d0024385df0c32a3f5aba2
Gary Mize <garymize@yahoo.com>
2023-06-06 14:00:15 -0600

    pw encryption

--- app/Models/users.php
+++ app/Models/users.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use \App\Traits\Encryptable;
+//use \App\Traits\Encryptable;
 
 class users extends Model
 {
     use HasFactory;
-    use Encryptable;
+    //use Encryptable;
     
     protected $table = 'users';
     protected $primaryKey = 'id';
     
-    protected $encryptable = [
-        'password'
-    ];
+//    protected $encryptable = [
+//        'password'
+//    ];
     
 }
