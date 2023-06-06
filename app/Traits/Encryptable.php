279238b645c2e32cfe580f625597b148be2a0c1c
Gary Mize <garymize@yahoo.com>
2023-06-06 14:18:05 -0600

    pw encryption

--- app/Traits/Encryptable.php
+++ app/Traits/Encryptable.php
@@ -12,12 +12,12 @@ trait Encryptable
 //        }
 //    }
 //
-//    public function setAttribute($key, $value)
-//    {
-//        if (in_array($key, $this->encryptable)) {
-//            $value = Crypt::encrypt($value);
-//        }
-//
-//        return parent::setAttribute($key, $value);
-//    }
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, $this->encryptable)) {
+            $value = Crypt::encrypt($value);
+        }
+
+        return parent::setAttribute($key, $value);
+    }
 }
