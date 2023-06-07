4661acd009e06b561308f78a403924702f99345e
Gary Mize <garymize@yahoo.com>
2023-06-07 15:42:54 -0600

    user change pw

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -135,7 +135,7 @@ class UsersController extends Controller
         
     }
     
-    public function updateNewPW(StoreusersRequest $request, $email){
+    public function updateNewPW($email, StoreusersRequest $request){
         
         $this->validate($request,[
 //            'email' => 'bail|required',
