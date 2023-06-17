918e45294b0242a616601b7a35c14f3a6de4d7ca
Gary Mize <garymize@yahoo.com>
2023-06-17 14:01:27 -0600

    LoginRequest, userAuth() / debug_to_console AuthenticatedSessionController

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,6 +17,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
+        debug_to_console('forms.login');
         return view('forms.login');
     }
 
