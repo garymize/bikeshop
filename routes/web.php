8700d52e3631d0a8889e7c9bde88fa5cd252a073
Gary Mize <garymize@yahoo.com>
2023-06-17 11:57:35 -0600

    LoginRequest, userAuth()

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,10 @@ Route::get('/home', function () {
 //    return view('dashboard');
 //})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
