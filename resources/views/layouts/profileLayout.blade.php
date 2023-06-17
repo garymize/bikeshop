42f63821fdcea16040c4660ecbee81bc582f9241
Gary Mize <garymize@yahoo.com>
2023-06-16 19:57:39 -0600

    login button

--- resources/views/layouts/profileLayout.blade.php
+++ resources/views/layouts/profileLayout.blade.php
@@ -1,3 +1,8 @@
+<?php
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }} style="--vh: 8.2px">
     <head>
