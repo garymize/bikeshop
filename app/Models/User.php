ec2c706bbe6e60cf68b5e0f01debcff2e5fb4a62
Gary Mize <garymize@yahoo.com>
2023-06-18 12:31:16 -0600

    LoginController

---
+++ app/Models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+//use \App\Traits\Encryptable;
+use Illuminate\Support\Facades\Crypt;
+
+class User extends Model
+{
+    use HasFactory;
+//    use Encryptable;
+    
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    
+    public function setpasswordAttribute($value)
+    {
+        $this->attributes['password'] = Crypt::encryptString($value);
+    }
+    
+    public function getpasswordAttribute($value)
+    {
+        try {
+            return Crypt::decryptString($value);
+        } catch (\Exception $e) {
+            return $value;
+        }
+    }
+    
+//    protected $encryptable = [
+//        'password'
+//    ];
+    
+}
