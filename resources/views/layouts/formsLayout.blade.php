4f51da03b8518f23d8ad05f63e24ad544ecf55a5
Gary Mize <garymize@yahoo.com>
2023-06-25 12:13:01 -0600

    favicon

--- resources/views/layouts/formsLayout.blade.php
+++ resources/views/layouts/formsLayout.blade.php
@@ -12,6 +12,8 @@
 
         <title>@yield('title')</title>
 
+        <link rel="icon" href="{{ url('images/favicon.webp') }}">
+        
         <!-- Fonts -->
         <!--<link rel="preconnect" href="https://fonts.bunny.net">-->
         <!--<link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />-->
