a0ad0f95afc1eedaa4c8125393bd82639282e884
Gary Mize <garymize@yahoo.com>
2023-06-25 11:45:07 -0600

    app name in forms layout

--- resources/views/layouts/formsLayout.blade.php
+++ resources/views/layouts/formsLayout.blade.php
@@ -10,7 +10,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>App Name - @yield('title')</title>
+        <title>@yield('title')</title>
 
         <!-- Fonts -->
         <!--<link rel="preconnect" href="https://fonts.bunny.net">-->
