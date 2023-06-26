8655c220d5b579e261baa6f6761bf21cd8e0d7fb
Gary Mize <garymize@yahoo.com>
2023-06-26 17:24:20 -0600

    input css text size

--- resources/views/layouts/formsLayout.blade.php
+++ resources/views/layouts/formsLayout.blade.php
@@ -12,28 +12,26 @@
 
         <title>@yield('title')</title>
 
-        <link rel="icon" href="{{ url('images/favicon.webp') }}">
-        
         <!-- Fonts -->
         <!--<link rel="preconnect" href="https://fonts.bunny.net">-->
         <!--<link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />-->
 
         <!-- Styles -->
         
-        <!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
-        <!--<link rel="stylesheet" type="text/css" href="/css/tooltipster.main.min.css" />-->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" type="text/css" href="/css/tooltipster.main.min.css" />
         <!--<link rel="stylesheet" type="text/css" href="/css/igs.css" />-->
-        <!--<link rel="stylesheet" type="text/css" href="/css/shopMain.css" />-->
+        <link rel="stylesheet" type="text/css" href="/css/shopMain.css" />
         <!--<link rel="stylesheet" type="text/css" href="/css/slm.css" />-->
         <!--<link rel="stylesheet" type="text/css" href="/css/igsDialog.css" />-->
         
         <!-- Scripts -->
         
-<!--        <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
         <script src="/js/tooltipster.main.min.js"></script>
         <script src="/js/tooltipster.bundle.js" ></script>
-        <script src='/js/slm.js'></script>-->
+        <script src='/js/slm.js'></script>
     <script type="text/javascript"> 
     function display_c(){
         var refresh=1000; // Refresh rate in milli seconds
