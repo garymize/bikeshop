69ab0c56a69d8000c8498ed8d481e7594e775483
Gary Mize <garymize@yahoo.com>
2023-06-26 16:47:42 -0600

    input css text size

--- resources/views/layouts/formsLayout.blade.php
+++ resources/views/layouts/formsLayout.blade.php
@@ -20,8 +20,8 @@
 
         <!-- Styles -->
         
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" type="text/css" href="/css/tooltipster.main.min.css" />
+        <!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+        <!--<link rel="stylesheet" type="text/css" href="/css/tooltipster.main.min.css" />-->
         <!--<link rel="stylesheet" type="text/css" href="/css/igs.css" />-->
         <link rel="stylesheet" type="text/css" href="/css/shopMain.css" />
         <!--<link rel="stylesheet" type="text/css" href="/css/slm.css" />-->
