64d604e1ea4a80e5f9d3f8943cad7a542b8329f6
Gary Mize <garymize@yahoo.com>
2023-06-28 13:20:49 -0600

    login page issues

--- resources/views/layouts/formsLayout.blade.php
+++ resources/views/layouts/formsLayout.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>@yield('title')</title>
+        @include('view.component.favicon')
 
         <!-- Fonts -->
         <!--<link rel="preconnect" href="https://fonts.bunny.net">-->
