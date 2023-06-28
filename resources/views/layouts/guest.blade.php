3d5a606aeb5795ad81347a4bfcd3603bf0fab7e0
Gary Mize <garymize@yahoo.com>
2023-06-28 17:03:56 -0600

    veridy- email.blade - logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
             <div>
                 <a href="/">
                     <!--<x-application-logo class="w-20 h-20 fill-current text-gray-500" />-->
-                    <img src='images/dhc-logo.webp' />
+                    <img src='/images/dhc-logo.webp' />
                 </a>
             </div>
 
