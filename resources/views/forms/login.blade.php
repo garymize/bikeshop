a61a8601bda523febd657a062a285a4c101d8176
Gary Mize <garymize@yahoo.com>
2023-06-28 13:08:52 -0600

    login page issues

--- resources/views/forms/login.blade.php
+++ resources/views/forms/login.blade.php
@@ -25,8 +25,8 @@
             <label for='email' class='required innerLabel'>Email</label><br>
             <input id="email" type='text' name="email" placeholder='' size='50' class='std-field-width' autocomplete="off" required/> <br>
             <label for='password' class='required innerLabel'>Password</label><br>        
-            <input id="password" type='password' name="password" placeholder='' size='50' class='std-field-width' autocomplete="off" required readonly onfocus="this.removeAttribute('readonly');"/> <br><br>
-            <input type='submit' value="Login" class="three-d-button" style="line-height: 38px; font-size: 28px; margin:0 auto; display: block; margin-top: 20px; border-color:white; border-width: thin;" readonly onfocus="this.removeAttribute('readonly');"/>
+            <input id="password" type='password' name="password" placeholder='' size='50' class='std-field-width' autocomplete="off" required readonly onfocus="this.removeAttribute('readonly');" readonly onfocus="this.removeAttribute('readonly');"/> <br><br>
+            <input type='submit' value="Login" class="three-d-button" style="line-height: 38px; font-size: 28px; margin:0 auto; display: block; margin-top: 20px; border-color:white; border-width: thin;"/>
             <!--<input type='button' style='display:inline-block; float:right;' formaction="{{ route('dashboard') }}" value="Cancel"/>-->
         </form>
     </div>
