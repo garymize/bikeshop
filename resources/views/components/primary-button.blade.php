46464e4da7aa793fed0610e362d8a084d6126a92
Gary Mize <garymize@yahoo.com>
2023-06-28 21:26:57 -0600

    verify email blade

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'three-d-button red-button verify-email-button']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'three-d-button red-button verify-email-button', 'style' => 'line-height: 39px; width: 255px; font-size: 20px;']) }}>
     {{ $slot }}
 </button>
