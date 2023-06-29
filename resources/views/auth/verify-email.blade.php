27b92146c42b8b6ad486a14ec32ade65d10e754b
Gary Mize <garymize@yahoo.com>
2023-06-28 21:09:50 -0600

    verify email blade

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <div style='margin-bottom: 20px;'>
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        <div style='font-size: 18px;'>
+            {{ __('Email Sent: Please Verify your Email Adress') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+            <div style='font-size: 18px;'>
                 {{ __('A new verification link has been sent to the email address you provided during registration.') }}
             </div>
         @endif
