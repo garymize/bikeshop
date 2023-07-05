16db38098123634321e9f0ed482fdcaf59ed21f8
Gary Mize <garymize@yahoo.com>
2023-07-05 16:53:37 -0600

    login auth::attempt

--- routes/web.php
+++ routes/web.php
@@ -36,13 +36,9 @@ Route::get('/home', function () {
     return view('home');
 });
 
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
