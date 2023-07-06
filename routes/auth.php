53449633e435aa6eb36c023acc563873ffd0d863
Gary Mize <garymize@yahoo.com>
2023-07-06 10:22:27 -0600

    login auth::attempt

--- routes/auth.php
+++ routes/auth.php
@@ -50,7 +50,7 @@ Route::middleware('auth')->group(function () {
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
                 ->name('password.confirm');
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'storex']);
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
