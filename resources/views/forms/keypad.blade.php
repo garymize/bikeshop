b2d5943a3788aba12e801ef2ebb9b3ebd79b2412
Gary Mize <garymize@yahoo.com>
2023-07-18 15:31:50 -0600

    keypad pinpad

--- resources/views/forms/keypad.blade.php
+++ resources/views/forms/keypad.blade.php
@@ -28,16 +28,10 @@
             <input type="password" class="keypad" disabled="disabled" />        </form>
             <div id="keypad">
                 <div id="keypad" class="keypad">
-                    <button class="number">1</button><button class="number">2</button><button class="number">3</button>
-                    <button class="number">4</button>
-                    <button class="number">5</button>
-                    <button class="number">6</button>
-                    <button class="number">7</button>
-                    <button class="number">8</button>
-                    <button class="number">9</button>
-                    <button class="delete">del</button>
-                    <button class="number">0</button>
-                    <button class="submit">ok</button>
+                    <button class="number">1</button>
+                    <button class="number">2</button>
+                    <button class="number">3</button>
+                    <button class="number">4</button><button class="number">5</button><button class="number">6</button><button class="number">7</button><button class="number">8</button><button class="number">9</button><button class="delete">del</button><button class="number">0</button><button class="submit">ok</button>
                 </div>                
             </div>
             <br><br>
