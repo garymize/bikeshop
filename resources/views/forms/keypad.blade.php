1b0607330f9bfd307932c71e726e33f5f4a3f243
Gary Mize <garymize@yahoo.com>
2023-07-19 17:37:59 -0600

    keypad pinpad

--- resources/views/forms/keypad.blade.php
+++ resources/views/forms/keypad.blade.php
@@ -28,7 +28,7 @@
             $('#canvas').css('width','267');
             $('#canvas').css('text-align','center');
             $('#canvas').css({'position':'relative','top':'30px'});
-            $("#keypadcontainer").css('position':'relative','top':'33px');
+            $("#keypadcontainer").css({'position':'relative','top':'33px'});
             $('.delete').css('background', 'url("/images/backspace-black-small-2.png") no-repeat');
             $('.delete').css('height','49px' );
             $('.delete').text('');
