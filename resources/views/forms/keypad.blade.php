024d2231663f43587ef2aea6b6ae7a747fa1447b
Gary Mize <garymize@yahoo.com>
2023-07-19 09:55:02 -0600

    keypad pinpad

--- resources/views/forms/keypad.blade.php
+++ resources/views/forms/keypad.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Login')
 
 @section('mm-header')
-    <div>Login</div>
+    <div>Unlock</div>
 @stop
 
 @section('mm-content')
@@ -35,24 +35,28 @@
         <form method="POST" action="{{ route('authUser') }}" autocomplete="off" style='184px;'>
             {{ debug_to_console('login.blade.php: '.route('authUser')) }}
             @csrf
-            <input type="password" class="keypad" disabled="disabled" />        </form>
-            <div id="keypad">
-                <div id="keypad" class="keypad">
-<button class="number">1</button>
-<button class="number">2</button>
-<button class="number">3</button>
-<button class="number">4</button>
-<button class="number">5</button>
-<button class="number">6</button>
-<button class="number">7</button>
-<button class="number">8</button>
-<button class="number">9</button>
-<button class="delete">del</button>
-<button class="number">0</button>
-<button class="submit">ok</button>
-                </div>                
-            </div>
-            <br><br>
+            <input type="password" class="keypad" disabled="disabled" />        
+        </form>
+        <div id="keypad">
+            <div id="keypad" class="keypad">
+                <button class="number">1</button>
+                <button class="number">2</button>
+                <button class="number">3</button>
+                <button class="number">4</button>
+                <button class="number">5</button>
+                <button class="number">6</button>
+                <button class="number">7</button>
+                <button class="number">8</button>
+                <button class="number">9</button>
+                <button class="delete">del</button>
+                <button class="number">0</button>
+                <button class="submit">ok</button>
+            </div>                
+        </div>
+        <div style='text-align: center;'>
+            <a href='{{ route('login') }}'>Password Login<a/>
+        </div>
+        <br><br>
     </div>
 
 @stop
