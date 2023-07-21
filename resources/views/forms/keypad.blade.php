53a8b3c85696a84a050ff9c050c55cc166285dea
Gary Mize <garymize@yahoo.com>
2023-07-20 22:44:25 -0600

    keypad pinpad

--- resources/views/forms/keypad.blade.php
+++ resources/views/forms/keypad.blade.php
@@ -35,7 +35,7 @@
             $('.delete').css('border','none');
             $('.delete').css({'position':'relative','top':'-7px','left':'2px'});
             $('#keypad .submit').css({'position':'relative','top':'-26px','height':'36px'});
-            $('button:contains("0")').css({'position':'relative','top':'-25px', 'height':'35px'});
+            $('button:contains("0")').css({'position':'relative','top':'-27px', 'height':'35px'});
         });
     </script>
     
